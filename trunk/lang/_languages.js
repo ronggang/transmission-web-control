b13b4bcec5fa164ab0bb0216284f25639fda3776
ronggang.zhou@gmail.com <ronggang.zhou@gmail.com@66eef085-b213-380b-273a-a1613e5da5fd>
2013-01-30 01:16:41 +0000

    添加西班牙语言包，感谢来自西班牙的 Adrián González

--- trunk/lang/_languages.js
+++ trunk/lang/_languages.js
@@ -3,5 +3,6 @@ system.languages = {
 	"zh-CN":"简体中文|Chinese(Simplified)"
 	,"zh-TW":"繁體中文|Chinese(Traditional)"
 	,"en":"English"
-	,"ru":"Russian (Русский)"
+	,"ru":"Русский(Russian)"
+	,"es":"Español(Spanish)"
 };
