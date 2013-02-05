7c0c3d3b5ce96c815d12b889b6fbe3e5ebbe8ccb
ronggang.zhou@gmail.com <ronggang.zhou@gmail.com@66eef085-b213-380b-273a-a1613e5da5fd>
2013-02-05 01:39:45 +0000

    更新波兰语言包（感谢Daniel Kolek） 添加匈牙利语言包（感谢 Csaba）

--- trunk/script/easyui/locale/easyui-lang-pl.js
+++ trunk/script/easyui/locale/easyui-lang-pl.js
@@ -1,46 +1,47 @@
-if ($.fn.pagination){
-	$.fn.pagination.defaults.beforePageText = 'Page';
-	$.fn.pagination.defaults.afterPageText = 'of {pages}';
-	$.fn.pagination.defaults.displayMsg = 'Displaying {from} to {to} of {total} items';
+﻿if ($.fn.pagination){
+	$.fn.pagination.defaults.beforePageText = 'Strona';
+	$.fn.pagination.defaults.afterPageText = 'z {pages}';
+	
+	$.fn.pagination.defaults.displayMsg = 'Wyświetlanie {from} do {to} z {total} elementów';
 }
 if ($.fn.datagrid){
-	$.fn.datagrid.defaults.loadMsg = 'Processing, please wait ...';
+	$.fn.datagrid.defaults.loadMsg = 'Przetwarzanie, proszę czekać ...';
 }
 if ($.fn.treegrid && $.fn.datagrid){
 	$.fn.treegrid.defaults.loadMsg = $.fn.datagrid.defaults.loadMsg;
 }
 if ($.messager){
 	$.messager.defaults.ok = 'Ok';
-	$.messager.defaults.cancel = 'Cancel';
+	$.messager.defaults.cancel = 'Anuluj';
 }
 if ($.fn.validatebox){
-	$.fn.validatebox.defaults.missingMessage = 'This field is required.';
-	$.fn.validatebox.defaults.rules.email.message = 'Please enter a valid email address.';
-	$.fn.validatebox.defaults.rules.url.message = 'Please enter a valid URL.';
-	$.fn.validatebox.defaults.rules.length.message = 'Please enter a value between {0} and {1}.';
-	$.fn.validatebox.defaults.rules.remote.message = 'Please fix this field.';
+	$.fn.validatebox.defaults.missingMessage = 'To pole jest wymagane.';
+	$.fn.validatebox.defaults.rules.email.message = 'Proszę podać poprawny adres e-mail.';
+	$.fn.validatebox.defaults.rules.url.message = 'Proszę podać poprawny link.';
+	$.fn.validatebox.defaults.rules.length.message = 'Proszę podać wartość pomiędzy {0} a {1}.';
+	$.fn.validatebox.defaults.rules.remote.message = 'Proszę poprawić to pole.';
 }
 if ($.fn.numberbox){
-	$.fn.numberbox.defaults.missingMessage = 'This field is required.';
+	$.fn.numberbox.defaults.missingMessage = 'To pole jest wymagane.';
 }
 if ($.fn.combobox){
-	$.fn.combobox.defaults.missingMessage = 'This field is required.';
+	$.fn.combobox.defaults.missingMessage = 'To pole jest wymagane.';
 }
 if ($.fn.combotree){
-	$.fn.combotree.defaults.missingMessage = 'This field is required.';
+	$.fn.combotree.defaults.missingMessage = 'To pole jest wymagane.';
 }
 if ($.fn.combogrid){
-	$.fn.combogrid.defaults.missingMessage = 'This field is required.';
+	$.fn.combogrid.defaults.missingMessage = 'To pole jest wymagane.';
 }
 if ($.fn.calendar){
 	$.fn.calendar.defaults.weeks = ['S','M','T','W','T','F','S'];
 	$.fn.calendar.defaults.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 }
 if ($.fn.datebox){
-	$.fn.datebox.defaults.currentText = 'Today';
-	$.fn.datebox.defaults.closeText = 'Close';
+	$.fn.datebox.defaults.currentText = 'Dzisiaj';
+	$.fn.datebox.defaults.closeText = 'Anuluj';
 	$.fn.datebox.defaults.okText = 'Ok';
-	$.fn.datebox.defaults.missingMessage = 'This field is required.';
+	$.fn.datebox.defaults.missingMessage = 'To pole jest wymagane.';
 }
 if ($.fn.datetimebox && $.fn.datebox){
 	$.extend($.fn.datetimebox.defaults,{
