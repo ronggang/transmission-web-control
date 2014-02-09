d3752dd0b0b20d3ae09f7ec1d729f8aa1dd70ca1
ronggang.zhou@gmail.com <ronggang.zhou@gmail.com@66eef085-b213-380b-273a-a1613e5da5fd>
2014-02-09 06:22:26 +0000

    更新俄语及台湾正体语言包

--- lang/_languages.js
+++ lang/_languages.js
@@ -1,7 +1,7 @@
 ﻿// 设置当前可用的语言
 system.languages = {
 	"zh-CN":"简体中文|Chinese(Simplified)"
-	,"zh-TW":"繁體中文|Chinese(Traditional)"
+	,"zh-TW":"正體中文|Chinese(Traditional)"
 	,"en":"English"
 	,"ru":"Русский(Russian)"
 	,"es":"Español(Spanish)"
