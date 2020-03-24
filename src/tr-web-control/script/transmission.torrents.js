c1c2437a28dff69e0329c710cebb9ba3d2172e38
imebeh <gmebeh@gmail.com>
2020-03-25 02:46:19 +0800

    修复 磁力链任务状态的感叹号图标

--- src/tr-web-control/script/transmission.torrents.js
+++ src/tr-web-control/script/transmission.torrents.js
@@ -296,7 +296,11 @@ transmission.torrents = {
 			}
 
 			if (warnings.length == trackerStats.length) {
-				item["warning"] = warnings.join(";");
+				if ((warnings.join(";")).replace(/;/g,"") == ""){
+					item["warning"] = "";
+				} else {
+					item["warning"] = warnings.join(";");
+				}
 				// 设置下次更新时间
 				if (!item["nextAnnounceTime"])
 					item["nextAnnounceTime"] = trackerInfo.nextAnnounceTime;
